f4e19bd56888d5b08fa565a188fc87253486a6e5
syc <1101175200@qq.com>
2024-07-19 18:58:58 +0800

    update: 增加了外部 api 的责任链校验

--- core/src/main/java/org/sycamore/llmhub/core/handler/api/OuterApiRequestParamVerifyChainHandler.java
+++ core/src/main/java/org/sycamore/llmhub/core/handler/api/OuterApiRequestParamVerifyChainHandler.java
@@ -1,9 +1,18 @@
 package org.sycamore.llmhub.core.handler.api;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.sycamore.llmhub.core.ModelRequestCommand;
+import org.sycamore.llmhub.infrastructure.cache.CacheUtil;
+import org.sycamore.llmhub.infrastructure.cache.DistributedCache;
+import org.sycamore.llmhub.infrastructure.cache.StringRedisTemplateProxy;
+import org.sycamore.llmhub.infrastructure.dataobject.mapper.ModelMapper;
+import org.sycamore.llmhub.infrastructure.dto.resp.SelectModelServerInfoByKeyRespDTO;
 import org.sycamore.llmhub.infrastructure.exception.ClientException;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static org.sycamore.llmhub.infrastructure.exception.BaseErrorCode.*;
 
@@ -14,13 +23,32 @@ import static org.sycamore.llmhub.infrastructure.exception.BaseErrorCode.*;
  * @description: 外部api参数合法性校验
  */
 @Component
+@RequiredArgsConstructor
 public class OuterApiRequestParamVerifyChainHandler implements OuterApiRequestChainFilter<ModelRequestCommand> {
+
+    private final DistributedCache distributedCache;
+    private final ModelMapper modelMapper;
     @Override
     public void handler(ModelRequestCommand requestParam) {
         if (!requestParam.getRequestModel().isValid()) {
             throw new ClientException(REQUEST_BODY_VERIFY_ERROR);
         }
+        String apiKey = requestParam.getApiKey();
+        String model = requestParam.getRequestModel().getModel();
 
+        SelectModelServerInfoByKeyRespDTO modelServerInfo = distributedCache.safeGet(
+                CacheUtil.buildKey("modelServerInfo", apiKey, model),
+                SelectModelServerInfoByKeyRespDTO.class,
+                () -> {
+                    return modelMapper.selectModelServerInfoByKey(apiKey, model).get(0);
+                },
+                7,
+                TimeUnit.DAYS
+        );
+        if (Objects.isNull(modelServerInfo)){
+            throw new ClientException(MODEL_INFO_VERIFY_ERROR);
+        }
+        requestParam.setModelServerInfo(modelServerInfo);
     }
 
     @Override
