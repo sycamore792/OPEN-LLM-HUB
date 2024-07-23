3a8b654cb1317afff46e49159f62385c0e1f0f10
syc <1101175200@qq.com>
2024-07-23 17:42:34 +0800

    update: api 调用统计作缓存策略

--- admin/src/main/java/org/sycamore/llmhub/service/impl/ModelLogServiceImpl.java
+++ admin/src/main/java/org/sycamore/llmhub/service/impl/ModelLogServiceImpl.java
@@ -3,6 +3,8 @@ package org.sycamore.llmhub.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.sycamore.llmhub.infrastructure.cache.CacheUtil;
+import org.sycamore.llmhub.infrastructure.cache.DistributedCache;
 import org.sycamore.llmhub.infrastructure.dataobject.ModelLog;
 import org.sycamore.llmhub.infrastructure.dataobject.mapper.ModelLogMapper;
 import org.sycamore.llmhub.infrastructure.dto.resp.ApiRequestCountRespDTO;
@@ -10,6 +12,7 @@ import org.sycamore.llmhub.infrastructure.dto.resp.ModelDataRespDTO;
 import org.sycamore.llmhub.service.ModelLogServiceI;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: Sycamore
@@ -21,14 +24,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ModelLogServiceImpl extends ServiceImpl<ModelLogMapper, ModelLog> implements ModelLogServiceI {
 
+    private final DistributedCache distributedCache;
 
     @Override
     public ModelDataRespDTO apiRequestCount() {
-
-        ModelDataRespDTO modelDataRespDTO = new ModelDataRespDTO();
-        modelDataRespDTO.setApiRequestCount(this.baseMapper.apiRequestCount());
-        modelDataRespDTO.setApiTokensCost(this.baseMapper.apiTokensCost());
-        modelDataRespDTO.setApiModelUsedInfo(this.baseMapper.apiModelUsedInfo());
-        return modelDataRespDTO;
+        return distributedCache.safeGet(CacheUtil.buildKey("api_data"), ModelDataRespDTO.class, () -> {
+                    ModelDataRespDTO modelDataRespDTO = new ModelDataRespDTO();
+                    modelDataRespDTO.setApiRequestCount(this.baseMapper.apiRequestCount());
+                    modelDataRespDTO.setApiTokensCost(this.baseMapper.apiTokensCost());
+                    modelDataRespDTO.setApiModelUsedInfo(this.baseMapper.apiModelUsedInfo());
+                    return modelDataRespDTO;
+                },
+                1,
+                TimeUnit.HOURS
+        );
     }
 }
